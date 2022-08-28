45679121fea551b5c7595ac5508d72a4350c2c8b
root <root@DESKTOP-MFEBK96.localdomain>
2022-08-28 22:39:38 +0200

    redirigir stderr a stdout y stdout a null

--- auto-B2bR/auto-b2br.sh
+++ auto-B2bR/auto-b2br.sh
@@ -19,14 +19,14 @@ function	ctrl_c()
 function	ft_user42()
 {
 	id="user42"
-	test -f | cat /etc/group | grep "user42" | grep $user42
+	test -f | cat /etc/group | grep "user42" | grep $user42 > /dev/null 2>&1
 	if [ "$?" == "0" ]; then
 		echo -e "${greenColor}[!] '$id group'\t\tOK!${endColor}\n"
 	else
 		# Crear grupo 'user42'
-		addgroup user42
+		addgroup user42 > /dev/null 2>&1
 		# Añadir usuario a 'user42'
-		adduser $user42 user42
+		adduser $user42 user42 > /dev/null 2>&1
 	fi
 	((c++))
 }
@@ -34,68 +34,69 @@ function	ft_user42()
 function	ft_sudo()
 {
 	id="sudo"
-	test -f | dpkg -l | grep "sudo"
+	test -f | dpkg -l | grep "sudo" > /dev/null 2>&1
 	if [ $? == "0" ]; then
 		echo -e "${greenColor}[!] '$id'\t\t\tOK!${endColor}\n"
 	else
 		echo -e "${yellowColor}[!] Instalando '$id'${endColor}\n"
 		# Instalar sudo
-		apt install -y sudo
+		apt install -y sudo > /dev/null 2>&1
 	fi	
 	# Instala 'libpam-pwquality' (strong password policy)
-	apt install -y libpam-pwquality
+	apt install -y libpam-pwquality > /dev/null 2>&1
 	# Añadir usuario a 'sudo'
-	adduser $user42 sudo
+	adduser $user42 sudo > /dev/null 2>&1
 	# Crear directorio 'sudo'
-	mkdir /var/log/sudo
+	mkdir /var/log/sudo > /dev/null 2>&1
 	# Crear archivo 'sudo.log'
-	touch /var/log/sudo/sudo.log
+	touch /var/log/sudo/sudo.log > /dev/null 2>&1
 	# Copiar archivos de configuración
-	cp -b login.defs /etc/
-	cp -b sudo_config /etc/sudoers.d/
-	cp -b common-password /etc/pam.d/common-password
+	cp -b login.defs /etc/ > /dev/null 2>&1
+	cp -b sudo_config /etc/sudoers.d/ > /dev/null 2>&1
+	cp -b common-password /etc/pam.d/common-password > /dev/null 2>&1
 	# Reiniciar servicio 'sudo'
-	/etc/init.d/sudo restart
+	/etc/init.d/sudo restart > /dev/null 2>&1
 	((c++))
 }
 
 function	ft_sshd()
 {
 	id="sshd"
-	test -f | dpkg -l | grep "openssh-server"
+	test -f | dpkg -l | grep "openssh-server" > /dev/null 2>&1
 	if [ $? == "0" ]; then
 		echo -e "${greenColor}[!] '$id'\t\t\tOK!${endColor}\n"
 	else	
 		echo -e "${yellowColor}[!] Instalando '$id'${endColor}\n"
 		# Instalar servidor ssh
-		apt install -y openssh-server
+		apt install -y openssh-server > /dev/null 2>&1
 	fi	
 	# Copiar archivos de configuración
-	cp -b sshd_config /etc/ssh/sshd_config
+	cp -b sshd_config /etc/ssh/sshd_config > /dev/null 2>&1
 	# Reiniciar servicio 'sshd'
-	/etc/init.d/sshd restart
+	/etc/init.d/sshd restart > /dev/null 2>&1
 	((c++))
 }
 
 function	ft_ufw()
 {
 	id="ufw"
-	test -f | dpkg -l | grep "ufw"
+	test -f | dpkg -l | grep "ufw" > /dev/null 2>&1
 	if [ $? == "0" ]; then
 		echo -e "${greenColor}[!] '$id'\t\t\tOK!${endColor}\n"
 	else
 		echo -e "${yellowColor}[!] Instalando '$id'${endColor}\n"
 		# Instalar 'ufw'
-		apt install -y ufw
+		apt install -y ufw > /dev/null 2>&1
 	fi
 	# Activar firewall 'ufw'
-	test -f | status ufw | grep -w "active"
+	test -f | status ufw | grep -w "active" > /dev/null 2>&1
 	if [ $? == "0" ]; then
 	# Permitir puerto '4242'
-	ufw allow 4242	
+	ufw allow 4242 > /dev/null 2>&1
 	else
 	# Habilitar 'ufw'
-	ufw enable
+	echo -e "${blueColor}[?] Activar firewall '$id' y terminar con la instalación [Y/n]: ${endColor}\n"
+	ufw enable > /dev/null 2>&1
 	fi
 	((c++))
 }
@@ -103,22 +104,23 @@ function	ft_ufw()
 function	ft_cron()
 {	
 	id="cron"
-	test -f | dpkg -l | grep " cron "
+	test -f | dpkg -l | grep " cron " > /dev/null 2>&1
 	if [ $? == "0" ]; then
 		echo -e "${greenColor}[!] '$id'\t\t\tOK!${endColor}\n"
 	else
 		echo -e "${yellowColor}[!] Instalando '$id'${endColor}\n"
 		# Instalar 'cron'
-		apt install cron -y
+		apt install cron -y > /dev/null 2>&1
 	fi
 	# Preparar archivos de configuración
-	crontab cron_config
+	crontab cron_config > /dev/null 2>&1
 	# Script 'monitoring.sh'
 	echo "echo crontab_OK!" > monitoring.sh
-	cp -b monitoring.sh /usr/local/bin
-	chmod 777 /usr/local/bin/monitoring.sh
+	cp -b monitoring.sh /usr/local/bin > /dev/null 2>&1
+	chmod 777 /usr/local/bin/monitoring.sh > /dev/null 2>&1
 	# Reiniciar servicio 'cron'
-	systemctl restart cron
+	systemctl restart cron > /dev/null 2>&1
+	fi
 	((c++))
 }
 
@@ -136,8 +138,8 @@ function	ft_install()
 	ft_user42
 	ft_sudo
 	ft_sshd
-	ft_ufw
 	ft_cron
+	ft_ufw
 	if [ $c == "5" ]; then
 		echo -e "${yellowColor}Instalacion completada${endColor}\n"
 	fi
